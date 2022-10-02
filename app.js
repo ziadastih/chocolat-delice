887af0a5c25d3ff190475e5ca18a706375a5fe99
ziad astih <ziadastih12@outlook.com>
2022-10-02 14:51:21 +0300

    done with truffles heaven for mobile now will manage it for other devices and did the slider

--- app.js
+++ app.js
@@ -18,6 +18,26 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 1000);
 });
 
+// ===============================carousel event listener =====================
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+let counter = 0;
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+prevBtn.style.opacity = 0;
+
+// ====================================functions section ===============================
 const animateImages = () => {
   const imageContainer = document.querySelector(".image-container");
   let charachter = 0;
@@ -39,7 +59,7 @@ const animateSlogan = () => {
   const sloganText = document.querySelector(".slogan");
   const strSlogan = sloganText.innerText;
   const splitSlogan = strSlogan.split("");
-  console.log(splitSlogan);
+
   sloganText.innerHTML = "";
   for (let i = 0; i < splitSlogan.length; i++) {
     sloganText.innerHTML += `<span class = "slogan-span">${strSlogan[i]}</span>`;
@@ -58,3 +78,23 @@ const animateSlogan = () => {
     }
   }
 };
+
+function carousel() {
+  if (counter < slides.length - 1) {
+    nextBtn.style.opacity = 0.8;
+    nextBtn.style.display = "flex";
+  } else {
+    nextBtn.style.opacity = 0;
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.opacity = 0.8;
+    prevBtn.style.display = "flex";
+  } else {
+    prevBtn.style.opacity = 0;
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
